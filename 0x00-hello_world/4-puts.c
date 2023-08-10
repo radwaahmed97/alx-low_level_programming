b1d5c9c5228cdd44fbf741a413b8c911450dfac4
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-08-10 06:01:08 -0700

    code updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/* main function "entry point of programm"
- *runned successfully */
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-
-puts("\"Programming is like building a multilingual puzzle");
+puts("\Programming is like building a multilingual puzzle /n");
 return (0);
-
 }
