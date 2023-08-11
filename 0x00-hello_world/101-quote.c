e1234675af313c1936cbd3106de6b5c02817411d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-08-11 11:50:23 -0700

    updated task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: Always 1 if Success.
+ * Return: 1 if Success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
 
