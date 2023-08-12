f9f6a09161d8f9aeb6ce91425a747a4a6dd66a6a
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-08-11 20:52:09 -0400

    task 9 updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,15 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar((i % 10) + '0');
+		if (i == 9)
 		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			putchar(',' + ' ');
+			continue;
 		}
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
