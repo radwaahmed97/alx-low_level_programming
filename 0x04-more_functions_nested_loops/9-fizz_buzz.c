4a91068dc2d67db12c3e43d15ddec1ee214fb573
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-08-17 17:35:10 -0400

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i == 100)
+			continue;
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
