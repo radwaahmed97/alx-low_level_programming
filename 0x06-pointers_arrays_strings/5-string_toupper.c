09e9f55ce679053eb0b092f237879de61823520d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-08-23 14:48:45 -0400

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ *Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * *string_toupper - changes all lowercase letters of a string
+ *		to uppercase.
+ *@s: string to be changed it's case.
+ *Return: pointer to string s
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		_islower(s[i]) ? s[i] -= 32 : s[i];
+
+	return (s);
+}
