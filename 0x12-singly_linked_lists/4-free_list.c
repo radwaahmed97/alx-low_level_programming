048c01f3ab49e085513fa37bbcc03b8ae703dd10
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-09-21 20:05:09 -0400

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the list
+ */
+
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+
+		if (head->str)
+			free(head->str);
+
+		free(head);
+	}
+}
