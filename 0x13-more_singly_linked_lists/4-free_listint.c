051265b5b8457d07a33e8050cdd577453bfb719d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-09-26 14:55:36 -0400

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: address of first node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr1, *ptr2;
+
+	ptr1 = head;
+
+	while (ptr1 != NULL)
+	{
+		ptr2 = ptr1->next;
+		free(ptr1);
+		ptr1 = ptr2;
+	}
+}
+
