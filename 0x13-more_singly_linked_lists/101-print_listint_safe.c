45a946c7816748d15eeef49d1af4692f6e9531bd
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-09-26 17:41:57 -0400

    101-print_listint_safe.c modified

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,62 +1,88 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * _realloc - reallocates memory for an array of pointers
- *			to nodes in a linked list.
- * @list: old lis to append
- * @size: size of new list (one more old list)
- * @new: new node added to the list
- * Return: pointer to new list
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
-
-const listint_t **_realloc(const listint_t **list, size_t size,
-		const listint_t *new)
+size_t looped_listint_len(const listint_t *head)
 {
-	size_t index;
-	const listint_t **newls;
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-	newls = malloc(size * sizeof(listint_t *));
-	if (newls == NULL)
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		free(list);
-		exit(98);
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
-	for (index = 0; index < size - 1; index++)
-		newls[index] = list[index];
-	newls[index] = new;
-	free(list);
-	return (newls);
+
+	return (0);
 }
 
 /**
- * print_listint_safe -  prints a listint_t linked list.
- * @head: head of the list
- * Return: number of the nodes in list, exit with status 98 if fails
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t index, number = 0;
-	const listint_t **list = NULL;
+	size_t nodes, index = 0;
 
-	while (head != NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		for (index = 0; index < number; index++)
+		for (; head != NULL; nodes++)
 		{
-			if (head == list[index])
-			{
-				printf("-> [%p] %d\n", (void *)head,
-						head->n);
-				free(list);
-				return (number);
-			}
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		number++;
-		list = _realloc(list, number, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
 	}
-	free(list);
-	return (number);
-}
 
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
+}
