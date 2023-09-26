6b793d972d74cdec16a10d06f56c4ac6f91ec69d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-09-26 19:10:31 -0400

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,19 +8,21 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *present = head;
 	size_t number_of_nodes = 0;
 
-	while (head && head > head->next)
+	while (present != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		++number_of_nodes;
-	}
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-		++number_of_nodes;
+		printf("[%p] %d\n", (void *)present, present->n);
+		number_of_nodes++;
+
+		if (present->next >= present)
+		{
+			printf("-> [%p] %d\n", (void *)present->next,
+					present->next->n);
+			break;
+		}
+		present = present->next;
 	}
 	return (number_of_nodes);
 }
