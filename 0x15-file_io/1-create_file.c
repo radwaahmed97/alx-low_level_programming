95b2086d257be3846b0c3aa83136b74dd38ef256
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-10-02 14:31:32 -0400

    1-create_file.c modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int filedescript, w, r = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	filedescript = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
