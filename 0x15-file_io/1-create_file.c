fcabf6bf201bbc3cfe6507d1f98ec240e6727b65
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-10-02 14:36:27 -0400

    1-create_file.c modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		while (text_content)
+		while (text_content[r])
 			r++;
 		w = write(filedescript, text_content, r);
 		if (w != r)
